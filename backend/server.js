e293100bbd027a60731808dc55ea4394d487b77e
Sriram V <vineethsriram7@gmail.com>
2026-09-22 12:03:44 +0530

    Initial commit - Week9 app base

---
+++ backend/server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Backend container!'));
+app.listen(3000, () => console.log('Backend running on port 3000'));
